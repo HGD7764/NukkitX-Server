6ee2e13c62341fa647b8aaa4c4b71d6f0f0e475d
HGD7764 <65435258+HGD7764@users.noreply.github.com>
2020-07-06 18:06:44 +0800

    Add files via upload

---
+++ NukkitX_1.14_20191211113738 3/run.bat
@@ -0,0 +1,5 @@
+::http://www.mcbbs.net/thread-810442-1-1.html
+@echo off
+:start
+java -jar nukkit-1.0-SNAPSHOT.jar
+goto start
---
+++ NukkitX_1.14_20191211113738 3/run.sh
@@ -0,0 +1,2 @@
+#http://www.mcbbs.net/thread-810442-1-1.html
+sudo java -jar nukkit-1.0-SNAPSHOT.jar
